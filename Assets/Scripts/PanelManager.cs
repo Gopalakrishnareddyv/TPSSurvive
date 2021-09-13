a0c9c60296b22a33cab0752e58650a16dd6082cc
GOPALAKRISHNAREDDY <gopalakrishna.vintha@xcubelabs.com>
2021-09-13 14:18:32 +0530

    changes in UI

--- Assets/Scripts/PanelManager.cs
+++ Assets/Scripts/PanelManager.cs
@@ -7,15 +7,24 @@ public class PanelManager : MonoBehaviour
 {
     public GameObject main;
     public GameObject options;
+    public GameObject help;
     public void Main()
     {
         main.SetActive(true);
-        options.SetActive(true);
+        options.SetActive(false);
+        help.SetActive(false);
+    }
+    public void Help()
+    {
+        main.SetActive(false);
+        options.SetActive(false);
+        help.SetActive(true);
     }
     public void Options()
     {
         main.SetActive(false);
         options.SetActive(true);
+        help.SetActive(false);
     }
     public void PlayButton()
     {
