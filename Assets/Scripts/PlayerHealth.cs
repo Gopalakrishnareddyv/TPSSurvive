1ee9a7d1dc5678ed3f4688b8bac55d8b934d2523
GOPALAKRISHNAREDDY <gopalakrishna.vintha@xcubelabs.com>
2021-09-13 11:37:51 +0530

    Fixed light issues whne loading another scene

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
     public float health;
     public float currentHealth;
+    public Text healthText;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +19,7 @@ public class PlayerHealth : MonoBehaviour
     public void Damage(float damage)
     {
         currentHealth -= damage;
+        healthText.text = "Helath : " + currentHealth;
         if (currentHealth <= 0)
         {
             //Debug.Log("Player died");
